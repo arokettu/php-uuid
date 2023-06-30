c7c4402b91bd1f2905268c12a2583a3c0cdc0ea7
Anton Smirnov <sandfox@sandfox.me>
2023-06-30 04:01:08 +0300

    Use furo

--- docs/conf.py
+++ docs/conf.py
@@ -5,13 +5,22 @@ import os, sys
 sys.path.append(os.curdir)
 from conf_project import *
 
-# author = 'Anton Smirnov'
-copyright = '{} {}'.format(datetime.now().year, 'Anton Smirnov')
+author = 'Anton Smirnov'
+copyright = '{} {}'.format(datetime.now().year, author)
 language = 'en'
 
 html_title = project
-html_theme = 'sphinx_book_theme'
+html_theme = 'furo'
 templates_path = ["_templates"]
 html_sidebars = {
-    "**": ["navbar-logo.html", "rtd-version.html", "icon-links.html", "sbt-sidebar-nav.html"]
+    "**": [
+        "sidebar/brand.html",
+        "rtd-version.html",
+        "sidebar/search.html",
+        "sidebar/scroll-start.html",
+        "sidebar/navigation.html",
+        "sidebar/ethical-ads.html",
+        "sidebar/scroll-end.html",
+        "sidebar/variant-selector.html",
+    ]
 }
