a497d26c72e87b459fb1059ca114fbfa5428ae0d
Anton Smirnov <sandfox@sandfox.me>
2023-07-08 02:17:18 +0300

    32 bit base 32

--- src/Helpers/Base32.php
+++ src/Helpers/Base32.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace Arokettu\Uuid\Helpers;
 
+use Arokettu\Unsigned as u;
+
 /**
  * @internal
  */
@@ -11,55 +13,37 @@ final class Base32
 {
     public static function encode(string $hex): string
     {
-        return
-            str_pad(self::encode5bytes(substr($hex, 0, 2)), 2, '0', STR_PAD_LEFT) .
-            str_pad(self::encode5bytes(substr($hex, 2, 10)), 8, '0', STR_PAD_LEFT) .
-            str_pad(self::encode5bytes(substr($hex, 12, 10)), 8, '0', STR_PAD_LEFT) .
-            str_pad(self::encode5bytes(substr($hex, 22, 10)), 8, '0', STR_PAD_LEFT);
-    }
-
-    private static function encode5bytes(string $hex): string
-    {
-        if (PHP_INT_SIZE >= 8) {
-            $num = base_convert($hex, 16, 32);
-            return strtr(
-                $num,
-                'abcdefghijklmnopqrstuv',
-                'ABCDEFGHJKMNPQRSTVWXYZ',
-            );
+        if (extension_loaded('gmp')) {
+            $num = gmp_strval(gmp_init($hex, 16), 32);
         } else {
-            throw new \LogicException('Not implemented');
+            $num = u\to_base(u\from_hex($hex, 16), 32);
         }
+
+        $digits = strtr(
+            $num,
+            'abcdefghijklmnopqrstuv',
+            'ABCDEFGHJKMNPQRSTVWXYZ',
+        );
+        return str_pad($digits, 26, '0', STR_PAD_LEFT);
     }
 
     public static function decode(string $base32): string
     {
         $base32 = strtoupper($base32);
 
-        return
-            str_pad(self::decode5bytes(substr($base32, 0, 2)), 1, "\0", STR_PAD_LEFT) .
-            str_pad(self::decode5bytes(substr($base32, 2, 8)), 5, "\0", STR_PAD_LEFT) .
-            str_pad(self::decode5bytes(substr($base32, 10, 8)), 5, "\0", STR_PAD_LEFT) .
-            str_pad(self::decode5bytes(substr($base32, 18, 8)), 5, "\0", STR_PAD_LEFT);
-    }
+        $num = strtr(
+            $base32,
+            // alphabet + alternative representations
+            'ABCDEFGHJKMNPQRSTVWXYZ' . 'ILO',
+            'abcdefghijklmnopqrstuv' . '110',
+        );
 
-    private static function decode5bytes(string $base32): string
-    {
-        if (PHP_INT_SIZE >= 8) {
-            $num = strtr(
-                $base32,
-                // alphabet + alternative representations
-                'ABCDEFGHJKMNPQRSTVWXYZ' . 'ILO',
-                'abcdefghijklmnopqrstuv' . '110',
-            );
-            return str_pad(
-                base_convert($num, 32, 16),
-                \strlen($base32) === 2 ? 2 : 10,
-                '0',
-                STR_PAD_LEFT
-            );
+        if (extension_loaded('gmp')) {
+            $hex = gmp_strval(gmp_init($num, 32), 16);
         } else {
-            throw new \LogicException('Not implemented');
+            $hex = u\to_hex(u\from_base($num, 32, 16));
         }
+
+        return str_pad($hex, 32, '0', STR_PAD_LEFT);
     }
 }
