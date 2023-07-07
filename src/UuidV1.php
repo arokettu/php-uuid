6a5b2e413bddde59207f108a751eb3352bb9129f
Anton Smirnov <sandfox@sandfox.me>
2023-07-08 02:35:52 +0300

    Reorder

--- src/UuidV1.php
+++ src/UuidV1.php
@@ -17,13 +17,15 @@ final readonly class UuidV1 extends AbstractUuid implements Rfc4122Uuid, TimeBas
 
     public function getDateTime(): DateTimeImmutable
     {
-        if (PHP_INT_SIZE >= 8) { // 64 bit - a simple way
-            $timeLow = hexdec(substr($this->hex, 0, 8)); // first 4 bytes
-            $timeMid = hexdec(substr($this->hex, 8, 4)); // next 2 bytes
-            $timeHigh = hexdec(substr($this->hex, 12, 4)) & 0b0000_1111_1111_1111; // next 2 bytes, strip version
+        $timeLow = substr($this->hex, 0, 8); // first 4 bytes
+        $timeMid = substr($this->hex, 8, 4); // next 2 bytes
+        $timeHigh = substr($this->hex, 13, 3); // next 2 bytes, skip version
 
+        $tsHex = $timeHigh . $timeMid . $timeLow;
+
+        if (PHP_INT_SIZE >= 8) { // 64 bit - a simple way
             // 100-nanosecond intervals since midnight 15 October 1582 UTC
-            $ts = $timeHigh << 48 | $timeMid << 32 | $timeLow;
+            $ts = hexdec($tsHex);
             $tsS = intdiv($ts, 10_000_000) + Helpers\Constants::V1_EPOCH; // convert to unix timestamp
             $tsUs = intdiv($ts % 10_000_000, 10); // lose 1 decimal of precision
 
