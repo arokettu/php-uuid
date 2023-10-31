5123b7436e1bb258794e5fbf4ec47a0c14ff2273
Anton Smirnov <sandfox@sandfox.me>
2023-10-31 05:57:45 +0200

    V1 Sequence

---
+++ src/Sequences/UuidV1Sequence.php
@@ -0,0 +1,46 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Arokettu\Uuid\Sequences;
+
+use Arokettu\Clock\SystemClock;
+use Arokettu\Uuid\Node\Node;
+use Arokettu\Uuid\Uuid;
+use Arokettu\Uuid\UuidV1;
+use Generator;
+use Psr\Clock\ClockInterface;
+use Random\Engine\Secure;
+use Random\Randomizer;
+
+/**
+ * @implements UuidSequence<UuidV1>
+ */
+final class UuidV1Sequence implements UuidSequence
+{
+    private UuidV6Sequence $innerSequence;
+
+    public function __construct(
+        ?Node $node = null,
+        ClockInterface $clock = new SystemClock(),
+        Randomizer $randomizer = new Randomizer(new Secure()),
+    ) {
+        // V1 sequences should be very rare, so I don't want to optimize
+        $this->innerSequence = new UuidV6Sequence($node, $clock, $randomizer);
+    }
+
+    public function next(): Uuid
+    {
+        return $this->innerSequence->next()->toUuidV1();
+    }
+
+    /**
+     * @return Generator<int, UuidV1, void>
+     */
+    public function getIterator(): Generator
+    {
+        while (true) {
+            yield $this->next();
+        }
+    }
+}
