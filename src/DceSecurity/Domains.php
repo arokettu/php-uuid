ec2581ef8ac88625118f2e1de46ca553a6ebc2b3
Anton Smirnov <sandfox@sandfox.me>
2024-01-04 06:05:20 +0200

    Dce Domains

---
+++ src/DceSecurity/Domains.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Arokettu\Uuid\DceSecurity;
+
+final class Domains
+{
+    public const PERSON = 0;
+    public const GROUP = 1;
+    public const ORG = 2;
+}
