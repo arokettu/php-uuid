a1ecc074964a488cfb5fe05d83590c98103ad2e2
Anton Smirnov <sandfox+git@sandfox.me>
2024-05-13 05:06:26 +0300

    Do not leave empty test file

--- tests/NamespacesTest.php
+++ tests/NamespacesTest.php
@@ -4,8 +4,16 @@ declare(strict_types=1);
 
 namespace Arokettu\Uuid\Tests;
 
+use Arokettu\Uuid\Namespaces\UuidNamespace;
+use Arokettu\Uuid\UuidV1;
 use PHPUnit\Framework\TestCase;
 
 class NamespacesTest extends TestCase
 {
+    public function testNamespaces(): void
+    {
+        foreach (UuidNamespace::cases() as $namespace) {
+            self::assertInstanceOf(UuidV1::class, $namespace->getUuid());
+        }
+    }
 }
