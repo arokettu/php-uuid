ea4d85815b832054da6e6b90fa3efeb2f8c54b42
Anton Smirnov <sandfox@sandfox.me>
2024-06-27 19:15:14 +0300

    Should be an interface

--- src/Helpers/CachedClock.php
+++ src/Helpers/CachedClock.php
@@ -5,15 +5,16 @@ declare(strict_types=1);
 namespace Arokettu\Uuid\Helpers;
 
 use Arokettu\Clock\SystemClock;
+use Psr\Clock\ClockInterface;
 
 /**
  * @internal
  */
 trait CachedClock
 {
-    private static SystemClock $clock;
+    private static ClockInterface $clock;
 
-    private static function clock(): SystemClock
+    private static function clock(): ClockInterface
     {
         return self::$clock ??= new SystemClock();
     }
